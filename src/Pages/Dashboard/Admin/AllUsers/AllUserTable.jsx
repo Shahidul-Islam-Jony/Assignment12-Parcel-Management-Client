38a63ccded31030ac09ba60958c9a5b2cace0f90
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-29 02:17:04 +0600

    Admin Panel All Users route successfully created

---
+++ src/Pages/Dashboard/Admin/AllUsers/AllUserTable.jsx
@@ -0,0 +1,68 @@
+
+import PropTypes from "prop-types"
+import { useState } from "react";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import swal from "sweetalert";
+
+const AllUserTable = ({ user }) => {
+    const axiosSecure = useAxiosSecure();
+    const [totalBooked, setTotalBooked] = useState(0);
+    axiosSecure.get(`/myParcel?email=${user.email}`)
+        .then(res => {
+            // console.log(res.data);
+            const usersAllBooked = res.data;
+            setTotalBooked(usersAllBooked.length)
+        })
+
+    const handleMakeDeliveryMen = async (id) => {
+        console.log(id);
+        await axiosSecure.patch(`/modifyUser/${id}`, { type: 'deliveryMen' })
+            .then(res => {
+                console.log(res.data);
+                swal("Done!", "Now user is a delivery Men", "success")
+            })
+    }
+    const handleMakeAdmin = async (id) => {
+        console.log(id);
+        await axiosSecure.patch(`/modifyUser/${id}`, { type: 'admin' })
+            .then(res => {
+                console.log(res.data);
+                swal("Done!", "Now user is an Admin", "success")
+            })
+    }
+
+    return (
+        <div className="overflow-x-auto">
+            <table className="table text-lg">
+                {/* head */}
+                <thead>
+                    <tr className="text-lg">
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="w-48">{user.name}</td>
+                        <td className="w-56">{user.phoneNumber}</td>
+                        <td className="w-72">{totalBooked}</td>
+                        <td className="w-64">total amount</td>
+                        <td className="flex flex-col gap-2">
+                            <button onClick={() => handleMakeDeliveryMen(user._id)} className="btn btn-sm w-40 btn-primary">Make Delivery Man</button>
+                            <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-sm w-40 btn-primary">Make Admin</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+AllUserTable.propTypes = {
+    user: PropTypes.object
+}
+
+export default AllUserTable;
--- src/Pages/Dashboard/Admin/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/AllUsers/AllUsers.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import AllUserTable from "./AllUserTable";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
+
     const { data: allUsers } = useQuery({
         queryKey: ['allUsers'],
         queryFn: async () => {
@@ -12,39 +14,32 @@ const AllUsers = () => {
             return res.data;
         }
     })
-    console.log(allUsers);
+    // console.log(allUsers);
+
+
     return (
         <div>
             <div className="my-10">
                 <h2 className="text-3xl text-center font-bold">All users</h2>
             </div>
-            <div className="overflow-x-auto ml-10">
-                <table className="table text-lg">
+            <div className="overflow-x-auto ml-4">
+                <table className="table">
                     {/* head */}
                     <thead>
                         <tr className="text-lg">
-                            <th>User's Name</th>
-                            <th>Phone Number</th>
-                            <th>Number of user Booked</th>
-                            <th>Total Spent Amount</th>
+                            <th className="w-48">User's Name</th>
+                            <th className="w-56">Phone Number</th>
+                            <th className="w-72">Number of user Booked</th>
+                            <th className="w-64">Total Spent Amount</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            allUsers?.map(user => <tr key={user._id}>
-                                <td>{user.name}</td>
-                                <td>{user.phoneNumber}</td>
-                                <td>num of parcel</td>
-                                <td>total amount</td>
-                                <td className="flex flex-col gap-2">
-                                    <button className="btn btn-sm btn-primary">Make Delivery Man</button>
-                                    <button className="btn btn-sm btn-primary">Make Admin</button>
-                                </td>
-                            </tr>)
-                        }
                     </tbody>
                 </table>
+                {
+                    allUsers?.map(user => <AllUserTable user={user} key={user._id}></AllUserTable>)
+                }
             </div>
         </div>
     );
